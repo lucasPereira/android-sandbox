c58e45d6697c0d27fa31d49e1e492f776ce1fcb4
Lucas Pereira da Silva <lucas@dominiol.com.br>
2021-11-24 18:24:06 -0300

    Read Android key store

--- app/src/main/java/br/dev/lucas/sandbox/MainActivity.kt
+++ app/src/main/java/br/dev/lucas/sandbox/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.graphics.drawable.toBitmap
 import java.io.InputStream
+import java.security.KeyStore
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,6 +27,21 @@ class MainActivity : AppCompatActivity() {
         val bytes: ByteArray = inputStream.readBytes()
         log("${bytes.size} bytes")
         inputStream.close()
+    }
+
+    fun onReadKeyStoreClick(view: android.view.View) {
+        log("Test")
+        val store: KeyStore = KeyStore.getInstance("AndroidKeyStore")
+        store.load(null)
+        val aliases: Enumeration<String> = store.aliases()
+        log(aliases.toList().size)
+        while (aliases.hasMoreElements()) {
+            log(aliases.nextElement())
+        }
+        val entry1: KeyStore.Entry = store.getEntry("com.csg.federaldev", null)
+        val entry2: KeyStore.Entry = store.getEntry("5437d76b-d87a-3e0a-a96b-73087f98becf", null)
+        log(entry1)
+        log(entry2)
     }
 
 }
